58267a9605198db24a3aaefd5fcbcf11a81cb22d
minji2219 <200146@jnu.ac.kr>
2024-08-01 01:50:09 +0900

    feat: 백엔드 별로 baseurl 설정하기 위한 select 해더에 추가

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -2,6 +2,8 @@ import { QueryClient } from '@tanstack/react-query';
 import type { AxiosInstance, AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
+import { apiSessionStorage } from '@/utils/storage';
+
 const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   const instance = axios.create({
     timeout: 5000,
@@ -15,11 +17,11 @@ const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
 
   return instance;
 };
-
-export const BASE_URL = 'https://api.example.com';
+const baseURL = apiSessionStorage.get();
+export const BASE_URL = baseURL;
 // TODO: 추후 서버 API 주소 변경 필요
 export const fetchInstance = initInstance({
-  baseURL: 'https://api.example.com',
+  baseURL: baseURL,
 });
 
 export const queryClient = new QueryClient({
