024d1e12d3cf3399374565c96c4a45e24a977858
minji2219 <200146@jnu.ac.kr>
2024-08-04 17:25:41 +0900

    feat: 헤더에 인증 토큰 함께 보내는 인스턴스 추가

--- src/api/hooks/useAddWish.ts
+++ src/api/hooks/useAddWish.ts
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { BASE_URL, fetchInstance } from '../instance';
+import { BASE_URL, fetchAuthInstance } from '../instance';
 import type { ProductDetailRequestParams } from './useGetProductDetail';
 
 type Props = ProductDetailRequestParams;
@@ -8,7 +8,7 @@ type Props = ProductDetailRequestParams;
 export const addWishListPath = () => `${BASE_URL}/api/wishes`;
 
 export const addWishList = async ({ productId }: Props) => {
-  return fetchInstance.post(addWishListPath(), {
+  return fetchAuthInstance.post(addWishListPath(), {
     productId: productId,
   });
 };
@@ -20,7 +20,9 @@ export const useAddWishList = ({ productId }: Props) => {
       alert('관심 등록 완료');
     },
     onError: () => {
-      console.error('에러 발생');
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      console.log(error.response.data.message);
     },
   });
 };
